0a353c07b20547706d5d69f443aa9806b23ba8c7
BuraBure <nicolas@santiagolab.cl>
2017-05-25 12:00:03 -0400

    Add no-direct-mutation rule
    forbid direct this.state mutation on react components except
    on constructor

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 module.exports = {
   rules: {
     'forbid-component-props': require('./lib/rules/forbid-component-props'),
+    'no-direct-mutation-state': require('./lib/rules/no-direct-mutation-state'),
   },
 }
---
+++ tests/lib/rules/no-direct-mutation-state.js
@@ -0,0 +1,149 @@
+/**
+ * @fileoverview Prevent direct mutation of this.state
+ * @author David Petersen
+ * @author Nicolas Fernandez <@burabure>
+ */
+
+'use strict'
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-direct-mutation-state')
+const RuleTester = require('eslint').RuleTester
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+    jsx: true,
+  },
+}
+
+require('babel-eslint')
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions })
+ruleTester.run('no-direct-mutation-state', rule, {
+
+  valid: [{
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    return <div>Hello {this.props.name}</div>;',
+      '  }',
+      '});',
+    ].join('\n'),
+  }, {
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    var obj = {state: {}};',
+      '    obj.state.name = "foo";',
+      '    return <div>Hello {obj.state.name}</div>;',
+      '  }',
+      '});',
+    ].join('\n'),
+  }, {
+    code: [
+      'var Hello = "foo";',
+      'module.exports = {};',
+    ].join('\n'),
+  }, {
+    code: [
+      'class Hello {',
+      '  getFoo() {',
+      '    this.state.foo = \'bar\'',
+      '    return this.state.foo;',
+      '  }',
+      '}',
+    ].join('\n'),
+  }, {
+    code: [
+      'class Hello extends React.Component {',
+      '  constructor() {',
+      '    this.state.foo = "bar"',
+      '  }',
+      '}',
+    ].join('\n'),
+  }],
+
+  invalid: [{
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    this.state.foo = "bar"',
+      '    return <div>Hello {this.props.name}</div>;',
+      '  }',
+      '});',
+    ].join('\n'),
+    errors: [{
+      message: 'Do not mutate state directly. Use setState().',
+    }],
+  }, {
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    this.state.person.name= "bar"',
+      '    return <div>Hello {this.props.name}</div>;',
+      '  }',
+      '});',
+    ].join('\n'),
+    errors: [{
+      message: 'Do not mutate state directly. Use setState().',
+    }],
+  }, {
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    this.state.person.name.first = "bar"',
+      '    return <div>Hello</div>;',
+      '  }',
+      '});',
+    ].join('\n'),
+    errors: [{
+      message: 'Do not mutate state directly. Use setState().',
+    }],
+  }, {
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    this.state.person.name.first = "bar"',
+      '    this.state.person.name.last = "baz"',
+      '    return <div>Hello</div>;',
+      '  }',
+      '});',
+    ].join('\n'),
+    errors: [{
+      message: 'Do not mutate state directly. Use setState().',
+      line: 3,
+      column: 5,
+    }, {
+      message: 'Do not mutate state directly. Use setState().',
+      line: 4,
+      column: 5,
+    }],
+  },
+  /**
+   * Would be nice to prevent this too
+  , {
+    code: [
+      'var Hello = createReactClass({',
+      '  render: function() {',
+      '    var that = this;',
+      '    that.state.person.name.first = "bar"',
+      '    return <div>Hello</div>;',
+      '  }',
+      '});'
+    ].join('\n'),
+    errors: [{
+      message: 'Do not mutate state directly. Use setState().'
+    }]
+  }*/
+  ],
+})
